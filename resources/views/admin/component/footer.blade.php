953c99d962ce46fd8391b4be6d07e29b4212c082
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-12-05 19:55:28 +0700

    Change

--- resources/views/admin/component/footer.blade.php
+++ resources/views/admin/component/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white mt-5">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; {{App\Setting::where('slug','nama-toko')->get()->first()->description}} {{date('Y')}} | Created by <a href='#' title='Namira' target='_blank'>Namira</a>
+            <span>Copyright &copy; {{App\Setting::where('slug','nama-toko')->get()->first()->description}} {{date('Y')}} | Created by <a href='#' title='FAIZAL ABDULLAH' target='_blank'>FAIZAL ABDULLAH</a>
             </span>
         </div>
     </div>
