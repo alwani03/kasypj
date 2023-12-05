9e0f7b61b0b84691c859284ec8aae2b1b642aabc
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-12-05 08:47:00 +0700

    Update Applications

--- resources/views/admin/component/login.blade.php
+++ resources/views/admin/component/login.blade.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Admin Page</title>
-    <link href="{{asset('assets/img/favicon.ico')}}" rel="icon" type="image/ico">
+    <title>Login</title>
+    <link href="{{asset('assets/img/money.ico')}}" rel="icon" type="image/ico">
     <!-- Custom fonts for this template-->
     <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
