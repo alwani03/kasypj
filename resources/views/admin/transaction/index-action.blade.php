8f462bff3a4353a3e751df5a4bd6b59bfe852124
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-12-05 08:57:30 +0700

    Change button

--- resources/views/admin/transaction/index-action.blade.php
+++ resources/views/admin/transaction/index-action.blade.php
@@ -1,21 +1,20 @@
 <div class="row align-items-center">
     <a href="{{route('transaction.show',[$id])}}"
         class="btn btn-info btn-sm shadow-sm"
-        data-toggle="tooltip"
         data-placement="top"
         title="Detail">
         <i class="fa fa-eye"></i>
     </a>
+    &nbsp;
     <a href="{{route('transaction.edit',[$id])}}"
         class="btn btn-success btn-sm shadow-sm"
-        data-toggle="tooltip"
         data-placement="top"
         title="Edit">
         <i class="fa fa-pen"></i>
     </a>
+    &nbsp;
     <a href="{{route('transaction.destroy',[$id])}}"
         class="btn btn-danger btn-sm shadow-sm delete-data"
-        data-toggle="tooltip"
         data-placement="top"
         title="Delete">
         <i class="fa fa-times"></i>
